0b4f7864dc24d16e989e6f97e2a663c3791f7a39
swapnil <swapnilpande96@gmail.com>
2014-07-15 19:29:49 +0530

    Final code working with 6 test cases

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -8,15 +8,37 @@ namespace Bowling
 {
     public class Game
     {
+        int [] rolls = new int[25];
+        public int score = 0;
+        public int rollpointer = 0;
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            rolls[rollpointer++]=pins;
+            if (pins == 10)
+            {
+                rolls[rollpointer++] = 0; 
+            }
+           
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int iterator = 0;
+            for (iterator = 0; iterator < 20; iterator++)
+            {
+                score += rolls[iterator];
+                if (iterator % 2 == 0 && rolls[iterator] == 10 && rolls[iterator + 2] == 10) // consecutive strikes
+                    score += rolls[iterator + 2] + rolls[iterator + 4];
+                else if (iterator % 2 == 0 && rolls[iterator] == 10)        //counting the bonus for strike
+                    score += rolls[iterator + 2] + rolls[iterator + 3];
+                else if (iterator % 2 == 0 && (rolls[iterator] + rolls[iterator + 1]) == 10) //condition for spare
+                    score += rolls[iterator + 2];                
+            }
+            return score;
         }
+
+
+       
 
     }
 }
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -1,14 +1,106 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Bowling;
 namespace BowlingFixtures
 {
     [TestClass]
     public class GameFixture
     {
+        Game g = new Game();
         [TestMethod]
-        public void TestMethod1()
+        public void gutters()
         {
+            for (int i = 0; i < 20; i++)
+                g.Roll(0);
+
+            Assert.AreEqual(0, g.GetScore());
         }
+        [TestMethod]
+        public void neitherstrikenorspare()
+        {
+            g.Roll(5);
+            g.Roll(1);
+            g.Roll(2);
+            g.Roll(1);
+            g.Roll(8);
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(1);
+            Assert.AreEqual(21, g.GetScore());
+        }
+
+        [TestMethod]
+        public void perfectgame()
+        {
+            for (int i = 0; i < 12; i++)
+                g.Roll(10);
+
+            Assert.AreEqual(300, g.GetScore());
+        }
+
+       
+
+         [TestMethod]
+        public void withsomestrikes()
+        {
+            g.Roll(5);
+            g.Roll(1);
+            g.Roll(2);
+            g.Roll(3);
+            g.Roll(10);
+            g.Roll(8);
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(1);
+
+            Assert.AreEqual(41, g.GetScore());
+        }
+
+
+         [TestMethod]
+         public void spareatlast()
+         {
+             g.Roll(5);
+             g.Roll(1);
+             g.Roll(2);
+             g.Roll(3);
+             g.Roll(10);
+             g.Roll(8);
+             g.Roll(0);
+             g.Roll(3);
+             g.Roll(7);
+             Assert.AreEqual(47, g.GetScore());
+         }
+
+         [TestMethod]
+         public void allspares()
+         {
+             g.Roll(3);
+             g.Roll(7);
+             g.Roll(3);
+             g.Roll(7);
+             g.Roll(3);
+             g.Roll(7);
+             g.Roll(3);
+             g.Roll(7);
+             g.Roll(3);
+             g.Roll(7);
+             g.Roll(3);
+             g.Roll(7);
+             g.Roll(3);
+             g.Roll(7);
+             g.Roll(3);
+             g.Roll(7);
+             g.Roll(3);
+             g.Roll(7);
+             g.Roll(3);
+             g.Roll(7);
+             g.Roll(9);
+         
+
+             Assert.AreEqual(136, g.GetScore());
+         }
     }
+
+   
 }
